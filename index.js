e1c2bd969127c72ea2e342eb58088885f72f032f
Chance <chance.felkel@gmail.com>
2023-08-09 14:30:27 -0400

    Added round function

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const getComputerChoice = () => {
     return choices[Math.floor(Math.random() * 3)]
 }
 
+const round = (playerSelection, computerSelection) => {
+    switch (playerSelection.toLowerCase()) {
+        case 'rock':
+            if (computerSelection === 'Rock') {
+                return "It's a tie!"
+            } else if (computerSelection === 'Paper') {
+                return "You lose! Paper beats rock."
+            } else {
+                return "You win! Rock beats scissors."
+            }
+            break;
+        case 'paper':
+            if (computerSelection === 'Paper') {
+                return "It's a tie!"
+            } else if (computerSelection === 'Scissors') {
+                return "You lose! Scissors beats paper."
+            } else {
+                return "You win! Paper beats rock."
+            }
+            break;
+        case 'scissors':
+            if (computerSelection === 'Paper') {
+                return "You win! Scissors beats paper."
+            } else if (computerSelection === 'Scissors') {
+                return "It's a tie!"
+            } else {
+                return "You lose! Rock beats scissors."
+            }
+            break;
+    }
+}
